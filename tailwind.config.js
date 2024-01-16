a7c289a8db31a9a214b9761afedd4d97fb7b4d75
Mayonesio <jara.mayonesio@gmail.com>
2024-01-16 12:56:52 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-
-/** @type {import('tailwindcss').Config} */
-export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
