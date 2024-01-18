203289a5c2da9d1e0fd2b4803b97f9f0a6a62ffd
Mayonesio <jara.mayonesio@gmail.com>
2024-01-18 17:49:35 +0100

    Footer y formulario del mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Navbar from './componentes/Navbar'
+import Navbar from './componentes/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Product from './pages/Product'
 import Login from './pages/Login'
@@ -8,27 +8,12 @@ import Productounico from './pages/Productdetails'
 import Aboutus from './pages/Aboutus'
 import Home from './pages/Home'
 
-
-
 function App() {
 
   return (
     <BrowserRouter>
     <Navbar />
-      <div>
-        
-      </div>
-      <div 
-      className="overflow-hidden relative"
-      // className='max-w-lg'
-      >
-        <Carousel autoSlide='true'>
-          {slides.map((s)=> (
-            <img src={s}/>
-            // <img src={Img}/>
-          ))}
-        </Carousel>
-      </div>
+     
    
       <Routes>
         <Route path='/Product' element={<Product />} />
