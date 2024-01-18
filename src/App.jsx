4800a28327417ecc9a93079d3ac583e4bf1a711c
Mayonesio <jara.mayonesio@gmail.com>
2024-01-18 12:38:23 +0100

    subir slider

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,32 @@ import Navbar from './componentes/navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Product from './pages/productos'
 import Login from './pages/login'
-import Producto_unico from './pages/producto_unico'
+import Producto_unico from './pages/producto_unico';
+import Carousel from './componentes/Carousel';
+import Img from './componentes/slider1.jpg';
+
+const slides = [ Img ]
 
 
 function App() {
 
-
   return (
     <BrowserRouter>
     <Navbar />
+      <div>
+        
+      </div>
+      <div 
+      className="overflow-hidden relative"
+      // className='max-w-lg'
+      >
+        <Carousel autoSlide='true'>
+          {slides.map((s)=> (
+            <img src={s}/>
+            // <img src={Img}/>
+          ))}
+        </Carousel>
+      </div>
       <Routes>
         <Route path='/productos' element={<Product />} />
         <Route path='/login' element={<Login />} />
