f78e58859184505b8df3f1f78a5fc7055dbad2a1
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-01-18 13:06:35 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,19 @@ import Login from './pages/Login'
 import Productounico from './pages/Productdetails'
 import Aboutus from './pages/Aboutus'
 import Home from './pages/Home'
-
-
+import Cart from './components/Cart/Cart'; // Importa el componente del carrito
 
 function App() {
-
-
   return (
     <BrowserRouter>
-    <Navbar />
-   
+      <Navbar />
       <Routes>
         <Route path='/Product' element={<Product />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Productdetails' element={<Productounico />} />
         <Route path='/Aboutus' element={<Aboutus />} />
         <Route path='/Home' element={<Home />} />
+        <Route path='/Cart' element={<Cart />} /> {/* Nueva ruta para el carrito */}
       </Routes>
     </BrowserRouter>
   )
