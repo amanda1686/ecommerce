c79254588d52359ab5034bff9e815c23fee14e40
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-01-19 18:25:28 +0100

    Productos

--- src/componentes/Slider.jsx
+++ src/componentes/Slider.jsx
@@ -57,7 +57,7 @@ export default function Sliderproducts() {
 
           <Slider {...settings}>
             {data.map((product) => (
-              <div key={product.name} className='text-back mb-4 bg-white rounded-xl border-1 border-sky-950 shadow-lg shadow-indigo-400'>
+              <div key={product.name} className='text-black mb-4 bg-white rounded-xl border-1 border-sky-950 shadow-lg shadow-indigo-400'>
                 <div className='rounded-t-xl flex justify-center items-center'>
                   <img src={product.img} alt="" className='h-[150px] mt-10'/>
                 </div>
@@ -69,7 +69,7 @@ export default function Sliderproducts() {
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                       </svg>
-                      COMPRAR
+                      BUY
                     </button>
                   </a>
                 </div>
