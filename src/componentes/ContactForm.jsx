fb0ecbc520e57f79419222f0fe5a2126782f99bb
Mayonesio <jara.mayonesio@gmail.com>
2024-01-19 13:00:56 +0100

    formulario de contacto en footer funcional

--- src/componentes/ContactForm.jsx
+++ src/componentes/ContactForm.jsx
@@ -1,61 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    company: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes manejar el envío del formulario, por ejemplo, enviar los datos a un servidor o realizar otras acciones.
+    console.log('Formulario enviado:', formData);
+    // También puedes restablecer el estado del formulario después del envío si es necesario.
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      company: '',
+      message: '',
+    });
+  };
+
   return (
     <div>
-      <div class="flex flex-col items-center justify-center h-screen dark">
-        <div class="w-full max-w-md bg-[#032940] rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-bold text-gray-200 mb-4">Contact Form</h2>
+      <div className="flex flex-col items-center justify-center h-screen dark mt-0">
+        <div className="w-full max-w-md bg-[#032940] rounded-lg shadow-md p-6">
+          <h2 className="text-2xl font-bold text-gray-200 mb-4">Contacto</h2>
 
-            <form class="flex flex-wrap">
+          <form onSubmit={handleSubmit} className="flex flex-wrap">
             <input
-                type="text"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] mr-[2%]"
-                placeholder="Full Name"
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] mr-[2%]"
+              placeholder="Nombre"
             />
             <input
-                type="email"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
-                placeholder="Email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
+              placeholder="Email"
             />
             <input
-                type="number"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] mr-[2%]"
-                placeholder="Phone Number"
+              type="number"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] mr-[2%]"
+              placeholder="Teléfono"
             />
             <input
-                type="text"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
-                placeholder="Company Name"
-            />
-            <input
-                type="text"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] mr-[2%]"
-                placeholder="Job Title"
-            />
-            <input
-                type="date"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
-                placeholder="Date of Birth"
+              type="text"
+              name="company"
+              value={formData.company}
+              onChange={handleChange}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[48%] ml-[2%]"
+              placeholder="Empresa"
             />
             <textarea
-                name="message"
-                class="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-auto md:mb-auto md:w-full md:h-auto md:min-h-[100px] md:max-h-[100px] md:flex-grow md:flex-shrink md:flex-auto focus:bg-gray-md:focus:outline-none:focus:ring-blue-md:focus:border-transparent transition ease-in-out duration-fastest"
-                placeholder="Message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-auto md:mb-auto md:w-full md:h-auto md:min-h-[200px] md:max-h-[100px] md:flex-grow md:flex-shrink md:flex-auto focus:bg-gray-md:focus:outline-none:focus:ring-blue-md:focus:border-transparent transition ease-in-out duration-fastest"
+              placeholder="Tu mensaje"
             ></textarea>
 
             <button
-                type="submit"
-                class="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
+              type="submit"
+              className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
             >
-                Submit
+              Submit
             </button>
-            </form>
+          </form>
         </div>
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
