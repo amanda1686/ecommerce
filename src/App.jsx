d3ced3ac6de2a769eeab2726b116957f3ae1031a
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-01-21 20:03:58 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./componentes/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
-import Productounico from "./pages/Productdetails";
-import Aboutus from "./pages/Aboutus";
-import Home from "./pages/Home";
-// import CartPage from "./pages/CartPage"; // DZ
-import Footer from "./componentes/Footer";
-// import { CartProvider } from "./context/cartContext"; // DZ
+import React from 'react'
+import './App.css'
+import Navbar from './componentes/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import Productounico from './pages/Productdetails'
+import Aboutus from './pages/Aboutus'
+import Home from './pages/Home'
+import Footer from './componentes/Footer'
 
 function App() {
+
   return (
     <BrowserRouter>
-      {/* <CartProvider>
-        {" "}
-        Agrega el CartProvider aquí */}
-        <Navbar />
-        <Routes>
-          <Route path="/Product" element={<Product />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Productdetails" element={<Productounico />} />
-          <Route path="/Aboutus" element={<Aboutus />} />
-          <Route path="/Home" element={<Home />} />
-          {/* <Route path='/Cart' element={<CartPage />} />   ruta para CartPage DZ */}
-        </Routes>
-        <Footer />
-      {/* </CartProvider> */}
+    <Navbar />
+     
+   
+      <Routes>
+        <Route path='/Product' element={<Product />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Productdetails' element={<Productounico />} />
+        <Route path='/Aboutus' element={<Aboutus />} />
+        <Route path='/Home' element={<Home />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
