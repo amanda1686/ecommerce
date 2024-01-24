fc7c64eecedc1437ab8a997966d3acca577b2b9e
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-01-24 17:56:20 +0100

    json

--- src/componentes/CartButton.jsx
+++ src/componentes/CartButton.jsx
@@ -3,7 +3,7 @@ import ShoppingCart from '../../public/img/logo/shopping-cart.png';
 
 const CartButton = ({ onClick, cartCount }) => {
   return (
-    <button onClick={onClick} className=" font-semibold relative">
+    <button onClick={onClick} className="font-semibold relative">
       <img src={ShoppingCart} alt="" className="h-9 w-9" />
       {cartCount > 0 && (
         <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2">
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001', // Ajusta la ruta según tu configuración
+    },
+  },
 })
+
