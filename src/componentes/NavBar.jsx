c4b069a3ad7629da9084d93ebfe39913567186e8
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-01-25 02:10:02 +0100

    Update

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -91,7 +91,8 @@ export default function Navbar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <div>
-                  <CartButton onClick={() => setCartVisible(!isCartVisible)} />
+                  <CartButton onClick={() => setCartVisible(!isCartVisible)}
+                   />
                 </div>
                 <SearchBar />
                 <Menu as="div" className="relative ml-3">
