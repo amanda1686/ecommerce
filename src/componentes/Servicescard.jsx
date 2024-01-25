b4729275e34bd0c6ddf32436bbb2d20c126dc85f
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-01-25 14:16:29 +0100

    Update Service-Card

--- src/componentes/Servicescard.jsx
+++ src/componentes/Servicescard.jsx
@@ -3,13 +3,9 @@ import React, { useState } from 'react';
 import abuela from '../assets/imagenes/abuela.jpg';
 import charla from '../assets/imagenes/charla.jpg';
 import limpieza from '../assets/imagenes/limpieza.jpg';
-import xicon from '../../public/img/logo/xicon.png'
+import xicon from '../../public/img/logo/xicon.png';
 
-const images = [
-  abuela,
-  charla,
-  limpieza,
-];
+const images = [abuela, charla, limpieza];
 
 export default function Servicescard() {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -24,25 +20,25 @@ export default function Servicescard() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Aquí puedes manejar la lógica del formulario
+    // Lógica del formulario
     console.log('Formulario enviado');
-    closeModal(); // Cierra el modal después de enviar el formulario
+    closeModal();
   };
 
   return (
     <>
       <h1 className='text-center text-black text-4xl font-bold mt-10 gap-10 ml-auto py-5'>Servicios</h1>
-      <div className="flex justify-center items-center space-x-4">
+      <div className="flex flex-wrap justify-center items-center gap-4 md:gap-8 lg:gap-12">
         {images.map((imageUrl, index) => (
           <div
             key={index}
-            className="relative cursor-pointer group"
+            className="relative cursor-pointer group w-full md:w-1/2 lg:w-1/3"
             onClick={() => openModal(imageUrl)}
           >
             <img
               src={imageUrl}
               alt={`Imagen ${index + 1}`}
-              className="rounded-lg h-[300px] opacity-1 group-hover:opacity-50"
+              className="rounded-lg h-[300px] md:h-[400px] lg:h-[500px] w-full opacity-1 group-hover:opacity-50"
             />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
               <p className="text-[#032940] text-lg font-semibold transition duration-300 opacity-0 group-hover:opacity-100">
@@ -54,8 +50,8 @@ export default function Servicescard() {
       </div>
       <br />
 
-     {/* Modal */}
-     {selectedImage && (
+      {/* Modal */}
+      {selectedImage && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
           <div className="relative max-w-screen-md w-full bg-white p-8 rounded-lg flex flex-col items-center">
             <p className="text-2xl font-bold mb-4">
