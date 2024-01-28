84e13ee4d57de86edde196c634ee2e4efa0fa02a
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-01-29 00:03:17 +0100

    pasarela de pago completa

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,33 @@ import Aboutus from "./pages/Aboutus";
 import Home from "./pages/Home";
 import Footer from "./componentes/Footer";
 import { CartProvider } from "./context/CartContext";
-import Wish from "./pages/Wish"; 
+import Wish from "./pages/Wish";
 import SignUp from "./pages/SignUp";
-import Checkout from "./pages/Checkout";
-import Catalog from './componentes/Catalog';
+import Checkout from "./componentes/Checkout";
+import Catalog from "./componentes/Catalog";
 import Services from "./pages/Services";
 import Date from "./pages/Date";
 import NavRegister from "./componentes/NavRegister";
 
-
-
 function App() {
   return (
     <BrowserRouter>
-      <NavRegister/>
+      <NavRegister />
       <NavBar />
-        <CartProvider>
-      <Routes>
-        <Route path="/" element={<Home />} index />
-        <Route path="/Product" element={<Product />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Productdetails" element={<Productounico />} />
-        <Route path="/Aboutus" element={<Aboutus />} />
-        <Route path="/Wish" element={<Wish />} /> 
-        <Route path="/SignUp" element={<SignUp />} /> 
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/" element={<Catalog />} />
-        <Route path="/Services" element={<Services />} />
-        <Route path="/Date" element={<Date />} />
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Home />} index />
+          <Route path="/Product" element={<Product />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Productdetails" element={<Productounico />} />
+          <Route path="/Aboutus" element={<Aboutus />} />
+          <Route path="/Wish" element={<Wish />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/" element={<Catalog />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Date" element={<Date />} />
+        </Routes>
       </CartProvider>
       <Footer />
     </BrowserRouter>
